24d16b8d23bada95e9d356be022139ab44a7d254
georgeforoglou <64276781+georgeforoglou@users.noreply.github.com>
2023-10-22 21:58:19 +0300

    All good

--- Parser.py
+++ Parser.py
@@ -6,7 +6,7 @@ from datetime import datetime
 import plotly.express as px
 
 
-# Function to check if the date string matches the format YYYYMMDD
+# Function to check if the date string matches the format YYYYMMDD 
 def is_valid_date(date_string):
     try:
         datetime.strptime(date_string, '%Y%m%d')
